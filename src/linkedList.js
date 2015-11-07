e88c0d8fca42a5309abbe0c17325fc97d03f4ada
Polina Soshnin <psoshnin@wellesley.edu>
2015-11-07 11:05:01 -0800

    implement linkedlist

--- src/linkedList.js
+++ src/linkedList.js
@@ -4,12 +4,39 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var newNode = Node(value);
+    if (list.head === null) {
+      list.head = newNode;
+      list.tail = newNode;
+    } else {
+      list.tail.next = newNode;
+      list.tail = newNode;
+    }
   };
 
   list.removeHead = function(){
+    if (list.head == null) {
+      return null;
+    }
+    var remove = list.head;
+    list.head = list.head.next;
+    return remove.value;
   };
 
   list.contains = function(target){
+    if (list.head == null) {
+      return false;
+    }
+
+    return (function searchNodes(node) {
+      if (node.value === target) {
+        return true;
+      } else if (node.next === null) {
+        return false;
+      } else {
+        return searchNodes(node.next);
+      }
+    })(list.head);
   };
 
   return list;
@@ -24,6 +51,3 @@ var Node = function(value){
   return node;
 };
 
-/*
- * Complexity: What is the time complexity of the above functions?
- */
