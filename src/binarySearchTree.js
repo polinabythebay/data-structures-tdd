bfd5b591af4e801e4fa5f0b2f615075574a7c040
Polina Soshnin <psoshnin@wellesley.edu>
2015-11-07 12:15:11 -0800

    implement binary search tree

--- src/binarySearchTree.js
+++ src/binarySearchTree.js
@@ -1,7 +1,62 @@
-var BinarySearchTree = function(value){
+var BinarySearchTree = function(value) {
+  this.value = value;
+  this.left = null;
+  this.right = null;
 };
 
+BinarySearchTree.prototype.insert = function(value) {
+  var newTree = new BinarySearchTree(value);
 
-/*
- * Complexity: What is the time complexity of the above functions?
- */
+  (function insertTree(bTree) {
+    if (bTree.value < value) {
+      if (bTree.right === null) {
+        bTree.right = newTree;
+      } else {
+        insertTree(bTree.right)
+      }
+    } else {
+      if (bTree.left ===  null) {
+        bTree.left = newTree;
+      } else {
+        insertTree(bTree.left);
+      }
+    }
+  })(this);
+};
+
+BinarySearchTree.prototype.contains = function(target) {
+
+  return (function searchTree(bTree) {
+    if (bTree.value === target) {
+      return true;
+    } 
+    if (bTree.value < target) {
+      if (bTree.right === null) {
+        return false;
+      } else {
+        return searchTree(bTree.right);
+      }
+    } 
+    if (bTree.value > target) {
+      if (bTree.left === null) {
+        present = false;
+      } else {
+        searchTree(bTree.left);
+      }
+    }
+  })(this);
+};
+
+BinarySearchTree.prototype.depthFirstLog = function(callback) {
+
+  (function traverseTree(bTree) {
+    callback(bTree.value);
+    if (bTree.left != null) {
+      traverseTree(bTree.left);
+    }
+    if (bTree.right != null) {
+      traverseTree(bTree.right);
+    }
+  })(this);
+};
+
